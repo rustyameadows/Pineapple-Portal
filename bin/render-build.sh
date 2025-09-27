e0adf80842f7c18c2f73730a6427914f5666f91f
Rusty Meadows <rustyameadows@gmail.com>
2025-09-26 21:33:16 -0700

    trying to fix puppeteer on render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,11 @@
 set -o errexit
 
 bundle install
+
+PUPPETEER_CACHE_DIR="$PWD/.cache/puppeteer"
+export PUPPETEER_CACHE_DIR
+mkdir -p "$PUPPETEER_CACHE_DIR"
+
 npm install
 npx puppeteer browsers install chrome
 bundle exec rails assets:precompile
