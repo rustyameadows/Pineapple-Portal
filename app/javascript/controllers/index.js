38fc5da29e39714f49795d77dd816c51b11f6d00
Rusty Meadows <rustyameadows@gmail.com>
2025-09-29 17:27:28 -0700

    Cleanup flash / toast mesage styling

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,9 +5,11 @@ import QuestionnaireController from "controllers/questionnaire_controller"
 import QuestionAttachmentController from "controllers/question_attachment_controller"
 import GeneratedSegmentsController from "controllers/generated_segments_controller"
 import EventSidebarController from "controllers/event_sidebar_controller"
+import FlashToastController from "controllers/flash_toast_controller"
 
 application.register("decision-modal", DecisionModalController)
 application.register("questionnaire", QuestionnaireController)
 application.register("question-attachment", QuestionAttachmentController)
 application.register("generated-segments", GeneratedSegmentsController)
 application.register("event-sidebar", EventSidebarController)
+application.register("flash-toast", FlashToastController)
