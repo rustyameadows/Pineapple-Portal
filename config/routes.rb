541124015a6e2c8ef87bb880b59cac763ce220f7
Rusty Meadows <rustyameadows@gmail.com>
2025-12-03 10:00:03 -0800

    add delete user

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-resources :users, only: %i[index new create edit update]
+resources :users, only: %i[index new create edit update destroy]
 resources :users, only: [] do
   resources :avatar_assets, only: :create, module: :users
 end
